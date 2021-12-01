3c3970bcdc1f3ec8d08ad8ebbc7e60c078161b54
Zane <zaneshaw.home@gmail.com>
2021-12-01 17:38:41 +1100

    Challenge1

---
+++ challenge1/main.js
@@ -0,0 +1,30 @@
+const $ = require('jquery');
+const fs = require('fs')
+const readline = require('readline');
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('challenge1/input.txt'),
+    console: false
+});
+
+var depth = 0;
+var lastDepth = 0;
+var increases = 0;
+
+readInterface.on('line', function(line) {
+    depth = parseInt(line);
+
+    if (lastDepth != 0) {
+        if (depth > lastDepth) {
+            increases++;
+            console.log(`increased depth! (${depth}) ${increases}`);
+        } else if (depth < lastDepth) {
+            console.log(`decreased depth! (${depth})`);
+        } else {
+            console.log(`depth did not change! (${depth})`);
+        }
+    } else {
+        console.log(`first sample! (${depth})`);
+    }
+
+    lastDepth = depth;
+});
