b678699e5c8cc1be74d76b2a271d2d267cb0b85b
Zane <zaneshaw.home@gmail.com>
2021-12-03 09:58:55 +1100

    Finish day1 part2

---
+++ 2021/day1/part2/main.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const readline = require('readline');
+
+fs.readFile('2021/day1/part2/input.txt', 'utf8', (err, data) => {
+    var decreases = 0;
+    data = data.split('\r\n').map(data => parseInt(data));
+
+    for (var i = 0; i < data.length-2; i++) {
+        var currentSet = data[i] + data[i+1] + data[i+2];
+        var nextSet = data[i+1] + data[i+2] + data[i+3];
+        if (currentSet < nextSet) {
+            decreases++;
+        }
+        console.log(decreases);
+    }
+});
