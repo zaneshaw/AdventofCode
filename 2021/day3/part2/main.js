1a1c8d5cd5731c9a95f4ca9fe2b4bb22bd86cfe0
Zane <zaneshaw.home@gmail.com>
2022-04-30 11:11:47 +1000

    Complete day 3, part 2

---
+++ 2021/day3/part2/main.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+var powerConsumption;
+var gammaRate = "";
+var epsilonRate = "";
+
+fs.readFile('2021/day3/part1/input.txt', 'utf8', (err, data) => {
+    data = data.split('\r\n');
+
+    for (let i = 0; i < data[0].length; i++) {
+        var zeros = 0;
+        var ones = 0;
+        for (var j = 0; j < data.length; j++) {
+            data[i].split("");
+            if (data[j][i] == "0") {
+                zeros++;
+            } else {
+                ones++;
+            }
+        }
+        if (ones > zeros) {
+            gammaRate += "1";
+            epsilonRate += "0";
+        } else {
+            gammaRate += "0";
+            epsilonRate += "1";
+        }
+    }
+
+    powerConsumption = parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
+    console.log(powerConsumption);
+});
