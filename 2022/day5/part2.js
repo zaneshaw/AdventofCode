0eef3b48c9406f8915b4e26c2d88be02bd74c37a
Zane Shaw <admin@triangle100.com>
2022-12-05 17:53:49 +1100

    Complete Day 5 2022

---
+++ 2022/day5/part2.js
@@ -0,0 +1,51 @@
+module.exports = {
+	run(data) {
+		const parts = data.split("\r\n\r\n");
+		const rawStacks = parts[0].split("\r\n");
+		const rawInstructions = parts[1].split("\r\n");
+		const stacks = new Array(8);
+		for (let i = 0; i < 9; i++) {
+			stacks[i] = [];
+		}
+
+		rawStacks.forEach((row, i) => {
+			if (i < parts[0].split("\n").length - 1) {
+				row = row.replace(/(.{3})./g, "$1").match(/.{1,3}/g);
+				row.forEach((item, i) => {
+					row[i] = item.slice(1, -1);
+					stacks[i].push(row[i]);
+				});
+			}
+		});
+		// console.log(stacks);
+
+		stacks.forEach((stack) => {
+			for (let i = 0; i < stack.length; i++) {
+				if (stack[i].trim() == "") {
+					stack.splice(i, 1);
+					i--;
+				}
+			}
+		});
+		// console.log(stacks);
+
+		rawInstructions.forEach((instruction) => {
+			instruction = instruction.match(/\d+/g);
+			const quantity = instruction[0];
+			const start = instruction[1] - 1;
+			const end = instruction[2] - 1;
+
+			stacks[end].unshift(...stacks[start].slice(0, quantity));
+			for (let i = 0; i < quantity; i++) {
+				stacks[start].shift();
+			}
+		});
+
+		let str = "";
+		stacks.forEach((stack) => {
+			str += stack[0];
+		});
+
+		return str;
+	},
+};
